9a588f2a7a6d37c1c2e5a9403646a8f5f3d781c9
Marvin <marvin.willms@code.berlin>
2020-04-29 14:10:19 +0200

    + Loading Screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import LoadingScreen from 'react-loading-screen'
 
 import World from './components/Globe';
 import Feed from './components/Feed';
@@ -9,21 +10,36 @@ const App = () => {
   const [country, setCountry] = useState('')
   const [height, setHeight] = useState(window.innerHeight)
   const [width, setWidth] = useState(window.innerWidth)
+  const [loading, setLoading] = useState(true)
+
+  window.addEventListener('load', (event) => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  });
 
   return (
     <>
-        <World
-          setShowFeed={setShowFeed}
-          setCountry={setCountry}
-          country={country}
-          height={height}
-          width={width}
-        />
-        <Feed
-          country={country}
-          showFeed={showFeed}
-        />
-        <UpdateSize setHeight={setHeight} setWidth={setWidth}/>
+      <LoadingScreen
+        loading={loading}
+        bgColor='#000817'
+        spinnerColor='#9ee5f8'
+        textColor='#f0f0f0'
+        text='The Globe is ready soon'
+      >
+      </LoadingScreen>
+      <World
+        setShowFeed={setShowFeed}
+        setCountry={setCountry}
+        country={country}
+        height={height}
+        width={width}
+      />
+      <Feed
+        country={country}
+        showFeed={showFeed}
+      />
+      <UpdateSize setHeight={setHeight} setWidth={setWidth}/>
     </>
   );
 };
